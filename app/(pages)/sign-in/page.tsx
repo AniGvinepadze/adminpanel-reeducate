fef626de074487c0a7f995be4b6e1559607396ae
Ani <a.gvin3@gmail.com>
2025-05-15 18:49:31 +0400

    Feat (  Sign-In ) : Sign-in responsive

--- app/(pages)/sign-in/page.tsx
+++ app/(pages)/sign-in/page.tsx
@@ -1,4 +1,3 @@
-
 import { logo } from "@/app/assets";
 import SignInFormFields from "@/app/components/SignIn/SignInFormFields";
 // import LoginFormFields from "@/app/components/Login/LoginFormFields";
@@ -6,29 +5,40 @@ import Image from "next/image";
 import React, { Suspense } from "react";
 
 export default function SignIn() {
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
-    <div className="max-w-[1440px] w-full m-auto h-screen flex items-center justify-center p-5 text-LightGrey rounded-lg">
-      <div className="max-w-[580px] w-full bg-DarkGrey rounded-xl  text-gray-200 m-auto p-7 max-450:p-2 max-302:p-0">
-        <div className="w-full flex justify-center gap-1">
-          <Image src={logo} alt="logo" width={70} height={42} className="max-450:max-w-[38px] rounded-xl"/>
+      <div className="max-w-[1440px] w-full m-auto h-screen flex items-center justify-center p-5 text-LightGrey rounded-lg">
+        <div className="max-w-[580px] w-full bg-DarkGrey rounded-xl  text-gray-200 m-auto p-7 ">
+          <div className="w-full flex justify-center gap-5">
+            <Image
+              src={logo}
+              alt="logo"
+              width={70}
+              height={42}
+              className="max-450:max-w-[60px] rounded-xl max-350:max-w-[50px]"
+            />
+            <div className="flex gap-1">
+              <h2 className="text-3xl font-semibold  max-450:text-[26px] max-350:text-[23px] ">
+                re:
+              </h2>
+              <h2 className="text-3xl font-semibold max-450:text-[26px] rotate-180 max-450:mt-[7px] max-350:text-[23px] ">
+                educate
+              </h2>
+            </div>
+          </div>
+          <div className="max-w-[580px] flex flex-col gap-2 mt-16  w-full max-450:mt-9 ">
+            <div className="">
+              <h2 className="text-[30px] font-semibold max-450:text-2xl">
+                Sign-in
+              </h2>
 
-          <h2 className="text-3xl font-semibold  max-450:text-[26px] ">re:</h2>
-          <h2  className="text-3xl font-semibold max-450:text-[26px] rotate-180">educate</h2>
-        </div>
-        <div className="max-w-[580px] flex flex-col gap-2 mt-16  w-full max-450:mt-9 ">
-        <div className="">
-        
-            <h2 className="text-[30px] font-semibold max-450:text-2xl">Sign-in</h2>
-      
-          <div className="max-w-[600px] w-full">
-                <SignInFormFields   />
-            </div> 
+              <div className="max-w-[600px] w-full">
+                <SignInFormFields />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </Suspense>
   );
 }

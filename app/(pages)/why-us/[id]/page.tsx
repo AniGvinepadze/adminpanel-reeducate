e6b12ca4fb95c757c0f1495ac45478abcff82d1d
Ani <a.gvin3@gmail.com>
2025-05-19 17:06:03 +0400

    Feat ( WhyUs  ) : Put method

---
+++ app/(pages)/why-us/[id]/page.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { WhyUs } from "../page";
+import { axiosInstance } from "@/app/lib/axiosIntance";
+import { getCookie } from "cookies-next";
+
+export default function page() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const id = pathname.split("/").pop() || "";
+
+  const [whyUs, setwhyUs] = useState<WhyUs | null>(null);
+  const [formData, setFormData] = useState({
+    description: "",
+  });
+  const token = getCookie("accessToken") as string;
+
+  useEffect(() => {
+    async function fetchWhyUs() {
+      try {
+        const response = await axiosInstance.get(`/why-us/${id}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setwhyUs(response.data);
+        setFormData({
+          description: response.data.description,
+        });
+      } catch (error) {
+        console.error("Failed to load course", error);
+      }
+    }
+
+    fetchWhyUs();
+  }, [id, token]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await axiosInstance.patch(`/why-us/${id}`, formData, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      router.push("/why-us");
+    } catch (error) {
+      console.error("Failed to update course", error);
+    }
+  };
+
+  return (
+    <div className="bg-DarkGrey min-h-[600px] w-full rounded-lg shadow-xl p-7 max-700:p-4">
+      <h1 className="text-2xl font-bold mb-4">Edit Course</h1>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        {["description"].map((field) => (
+          <div key={field}>
+            <label
+              htmlFor={field}
+              className="block mb-1 font-medium capitalize p-1"
+            >
+              {field}
+            </label>
+            <input
+              id={field}
+              name={field}
+              value={(formData as any)[field]}
+              onChange={handleChange}
+              className="w-full border text-gray-700 border-gray-300 rounded-xl px-3 py-2"
+            />
+          </div>
+        ))}
+
+        <button
+          type="submit"
+          className="mt-6 px-4 py-2 w-full my-2 bg-MainBg text-base font-medium text-white rounded-lg hover:bg-[#0f0f0f] hover:scale-105 transition-all ease-in-out duration-300"
+        >
+          Save Changes
+        </button>
+      </form>
+    </div>
+  );
+}
--- app/(pages)/why-us/page.tsx
+++ app/(pages)/why-us/page.tsx
@@ -83,7 +83,7 @@ export default function page() {
           </button>
           <button
             className=" bg-MainBg rounded-xl flex justify-between gap-4 py-3 px-6 my-3 hover:scale-110 ease-in-out duration-300 transition-all"
-            onClick={() => router.push(`/courses/${el._id}`)}
+            onClick={() => router.push(`/why-us/${el._id}`)}
           >
             Edit
           </button>

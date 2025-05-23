45c4187dcd6c0d418e285b2eb05944fa18db65b5
Ani <a.gvin3@gmail.com>
2025-05-23 16:11:30 +0400

    Fix ( Feedback ) : change feedback design & responsive

--- app/(pages)/feedback/page.tsx
+++ app/(pages)/feedback/page.tsx
@@ -82,17 +82,18 @@ export default function page() {
   };
 
   return (
-    <div className=" bg-DarkGrey min-h-[600px] max-w-[1000px] w-full rounded-xl shadow-lg p-7 ">
+    <div className=" bg-DarkGrey min-h-[600px] max-w-[1000px] w-full rounded-xl shadow-lg p-7  max-600:p-3 ">
       <div className="w-full max-w-[1000px] flex justify-between p-3 relative">
         <div>
-          <h1 className="text-2xl font-bold mb-4">გამოხმაურებები</h1>
+          <h1 className="text-2xl font-bold mb-4 max-400:text-xl max-350:text-lg">გამოხმაურებები</h1>
         </div>
-        <button
-          className="bg-MainBg rounded-xl flex justify-between gap-4 py-3 px-6 hover:scale-110 ease-in-out duration-300 transition-all text-base font-medium
-            "
+       <button
+          className="bg-MainBg rounded-xl flex justify-between gap-4 py-3 px-6 hover:scale-110 ease-in-out duration-300 transition-all text-base font-medium max-500:py-1 max-350:px-4
+          "
           onClick={() => setIsAddModalOpen((prev) => !prev)}
         >
-          + დამატება
+         <p className="max-500: text-2xl max-350:text-lg">+</p>
+         <p className="max-500:hidden">დამატება</p>
         </button>
       </div>
       {isAddModalOpen && (
@@ -106,28 +107,23 @@ export default function page() {
         </div>
       )}
 
-      {feedback.map((el) => (
-        <div key={el._id} className="flex justify-between gap-3">
-          <div className="w-full bg-MainBg rounded-xl flex justify-between gap-4 py-3 px-10 my-3">
-            <p className="text-base font-medium">{el.description}</p>
-            <div className="text-base font-medium">
-              {el.user.map((u, index) => (
-                <div key={index}>
-                  <p>{u.fullName}</p>
-                  <p>{u.category}</p>
-                </div>
-              ))}
+       {feedback.map((el) => (
+        <div key={el._id} className=" flex flex-col gap-1">
+          <div className="w-full bg-MainBg rounded-xl flex flex-col gap-4 p-3 my-3">
+            <div className="max-w-[1000px] w-full">
+            <p className="text-sm font-medium p-2">Description </p>
+            <p className="text-base font-medium w-full bg-DarkGrey p-2 rounded-xl">{el.description} </p>
             </div>
           </div>
           <button
-            className=" bg-MainBg rounded-xl flex justify-between gap-4 py-3 px-6 my-3 hover:scale-110 ease-in-out duration-300 transition-all"
+            className=" bg-MainBg rounded-xl flex justify-center py-2 px-6  hover:scale-105 ease-in-out duration-300 transition-all"
             onClick={() => handleDelete(el._id)}
           >
             Delete
           </button>
           <button
-            className=" bg-MainBg rounded-xl flex justify-between gap-4 py-3 px-6 my-3 hover:scale-110 ease-in-out duration-300 transition-all"
-            onClick={() => router.push(`/about-us/${el._id}`)}
+            className=" bg-MainBg rounded-xl flex justify-center gap-4 py-2 px-6 my-1 hover:scale-105 ease-in-out duration-300 transition-all"
+            onClick={() => router.push(`/why-us/${el._id}`)}
           >
             Edit
           </button>
